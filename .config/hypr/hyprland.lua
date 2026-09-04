d289aaaddde5f4c91e02b897f0ba75aa2a51825a
hatop0pp0 <277675439+hatop0pp0@users.noreply.github.com>
2026-09-04 23:24:53 +0900

    Fix: remove deprecated enable_bash_integration from wezterm.lua

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -392,5 +392,5 @@ hl.window_rule({
 	move = "20 monitor_h-120",
 	float = true,
 })
-  '';
-}
+
+
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,4 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- Bashやシェルとの連携を綺麗にする設定です
-config.enable_bash_integration = true
-
 return config
